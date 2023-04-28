64ceaf2075037615ab7071b6776ff5900ad9eb82
JunkEye <89807197+JunkEye@users.noreply.github.com>
2023-04-28 13:53:45 -0400

    changes
    redoing app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,184 +1,236 @@
 App =
 {
-    // top level varaibles
-    web3: null,
-    contracts: {},
-    //development
-    url:'http://127.0.0.1:7545',
-    network_id:1337,
-    sender:null,
-    receiver:null,
-    value:1000000000000000000,
-    index:0,
-    margin:10,
-    left:15,
-    init: function()
+  web3: null,
+  contracts: {},
+  address: "",
+  network_id: 3, // 5777 for local
+  handler: null,
+  value: 1000000000000000000,
+  index: 0,
+  margin: 10,
+  left: 15,
+  init: function ()
+  {
+    return App.initWeb3();
+  },
+
+  initWeb3: function ()
+  {
+    if (typeof web3 !== "undefined")
     {
-      return App.initWeb3();
-    },
-    
-    initWeb3: function()
-    {   
-      //initializing web3      
-      if (typeof web3 !== 'undefined') {
-        App.web3 = new Web3(Web3.givenProvider);
-      } else {
-        App.web3 = new Web3(App.url);
-      }
-      ethereum.enable();      
-      return App.initContract();  
-    },
-
-    initContract: function()
-    {   
-      $.getJSON('snap-sc.json', function(data)
-      {       
-        App.contracts.Payment = new App.web3.eth.Contract(data.abi, data.networks[App.network_id].address, {});
-        //populating contract's balance
-        App.web3.eth.getBalance(App.contracts.Payment._address).then((res)=>{ jQuery('#channel_balance').text(App.web3.utils.fromWei(res),"ether");})   
-      }) 
-           
-      return App.bindEvents();
-    },  
-  
-    bindEvents: function()
-    {  
-      //Applicant registration
-      $(document).on('click', '#regapplicant', function()
-      {
-        App.regApp();
-      });
-
-      //Merchant registration
-      $(document).on('click', '#regmerchant', function()
-      {
-        App.regMer();
-      });
-
-      //Allows the merchant to set a magic number
-      $(document).on('click', '#setmagic', function()
-      {
-        App.setMagic(jQuery('#magicnum').val());
-      });
-
-      //Allows an applicant to request additional Succuleux
-      $(document).on('click', '#requestbutton', function()
-      {
-        App.requestTickets(jQuery('#requestamount').val());
-      });
-
-      //Allows anybody to transfer tickets to anybody
-      $(document).on('click', '#send', function()
-      {
-         App.transferTokens(jQuery('#receiveaddress').val(), jQuery('#sendamount').val());
-      });
-  
-      //Allows bureaucrat to approve Succuleux requests
-      $(document).on('click', '#approve', function()
-      {
-        App.approve(jQuery('#addressdecision').val(), jQuery('#amountdecision').val());
-      });
-
-      //Allows bureaucrat to deny Succuleux requests
-      $(document).on('click', '#deny', function()
-      {
-        App.deny(jQuery('#addressdecision').val());
-      });
-
-      App.populateAddress();
-    },
-
-    //Not sure WTF this does?
-    populateAddress : function()
-    {  
-        // getting sender and reciever address and their balances            
-        new Web3(App.url).eth.getAccounts((err, accounts) => {
-          if(!err)
-          {
-            App.receiver=accounts[1]; 
-            App.sender = accounts[0];         
-            jQuery('#receiver').val(App.receiver);
-            App.web3.eth.getBalance(accounts[0]).then((res)=>{ jQuery('#sender_balance').text(App.web3.utils.fromWei(res),"ether");});
-            App.web3.eth.getBalance(accounts[1]).then((res)=>{ jQuery('#receiver_balance').text(App.web3.utils.fromWei(res),"ether");});      
-        }
-        else
-        {
-          console.log('Something went wrong');
-        }
-          });
-    },  
-
-    regApp:function()
+      App.web3 = new Web3(Web3.givenProvider);
+    }
+    else
     {
-      App.contracts.Payment.methods.registerApplicant();
-    },
+      App.web3 = new Web3(App.url);
+    }
+    ethereum.request({ method: "eth_requestAccounts" });
+    return App.initContract();
+  },
 
-    regMer:function()
-    {
-      App.contracts.Payment.methods.registerMerchant();
-    },
+  initContract: function ()
+  {
+    App.contracts.snap_sc = new App.web3.eth.Contract(App.abi, App.address, {});
+    return App.bindEvents();
+  },
 
-    //Function for magic number to be set by merchants
-    setMagic:function(num)
-    {  
-      if(typeof(num) != typeof(1))
-      {
-        alert('Please correct the magic number.');
-        return false;
-      }
-      App.contracts.Payment.methods.setMagicNumber(num)
-    },
+  bindEvents: function () {
+    $(document).on("click", "#regapplicant", function () {
+      App.handleInitialization(jQuery("#Initialize").val());
+    });
 
-    //Function for users to request tickets from the bureaucrat
-    requestTickets:function(amount)
-    {
-      if(typeof(amount) != typeof(1))
-      {
-        alert('Please correct the requested amount.');
-        return false;
-      }
-      //Run the SC
-      App.contracts.Payment.methods.requestProvisionChange(amount)
-      
-      //Record off-chain
-      const fs = require('fs');
-      let data = App.contracts.Payment._address+" formally requests "+amount+" Succuleux.";
-      fs.appendFile('requests.txt', data, function (err)
-      {
-        if (err)
-        {
-          throw err;
-        }
-      });
-    },
-  
-    //Function for transferring tickets in any context besides approving provisions
-    transferTokens:function (address, amount)
-    {     
-      if(typeof(amount) != typeof(1))
-      {
-        alert('Please correct the amount');
-        return false;
-      }
-      App.contracts.methods.transferTickets(address, amount)
-    },
+    $(document).on("click", "#getCounter", function () {
+      App.handleGet();
+    });
+    $(document).on("click", "#incrementCounter", function () {
+      App.handleIncrement(jQuery("#Increment").val());
+    });
+    $(document).on("click", "#decrementCounter", function () {
+      App.handleDecrement(jQuery("#Decrement").val());
+    });
+    App.populateAddress();
+  },
 
-    //Function for bureaucrats approving requests
-    approve:function (address, amount)
-    {     
-      if(typeof(amount) != typeof(1))
-      {
-        alert('Please correct the amount');
-        return false;
-      }
-      App.contracts.methods.transferTickets(address, amount)
-    },
+  populateAddress: async function () {
+    const accounts = await App.web3.eth.getAccounts();
+    App.handler = accounts[0];
+  },
 
-    //Function for bureaucrats denying requests
-    deny:function (address)
-    {     
-      alert('Request Denied!');
+  handleInitialization: function (counterValue) {
+    if (counterValue === "") {
+      toastr.error("Please enter a valid initializing value.", "Reverted!");
       return false;
     }
-  }
-;
+    var option = { from: App.handler };
+    App.contracts.snap_sc.methods
+      .initialize(counterValue)
+      .send(option)
+      .on("receipt", (receipt) => {
+        toastr.success("Success! Counter is set to " + counterValue);
+      })
+      .on("error", (err) => {
+        toastr.error(App.getErrorMessage(err), "Reverted!");
+      });
+  },
+
+  handleGet: function () {
+    App.contracts.snap_sc.methods
+      .get()
+      .call()
+      .then((r) => {
+        jQuery("#counter_value").text(r);
+      })
+      .catch((err) => {
+        toastr.error(App.getErrorMessage(err), "Reverted!");
+      });
+  },
+
+  handleIncrement: function (incrementValue) {
+    if (incrementValue === "") {
+      toastr.error("Please enter a valid incrementing value.", "Reverted!");
+      return false;
+    }
+    var option = { from: App.handler };
+    App.contracts.snap_sc.methods
+      .increment(incrementValue)
+      .send(option)
+      .on("receipt", (receipt) => {
+        toastr.success("Success! Counter is incremented by " + incrementValue);
+      })
+      .on("error", (err) => {
+        toastr.error(App.getErrorMessage(err), "Reverted!");
+      });
+  },
+
+  handleDecrement: function (decrementValue) {
+    if (decrementValue === "") {
+      toastr.error("Please enter a valid decrementing value.", "Reverted!");
+      return false;
+    }
+    var option = { from: App.handler };
+    App.contracts.snap_sc.methods
+      .decrement(decrementValue)
+      .send(option)
+      .on("receipt", (receipt) => {
+        toastr.success("Success! Counter is decremented by " + decrementValue);
+      })
+      .on("error", (err) => {
+        toastr.error(App.getErrorMessage(err), "Reverted!");
+      });
+  },
+
+  getErrorMessage: function (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    let errorReason = "";
+
+    if (errorCode === 4001) {
+      return "User rejected the request!";
+    } else if (
+      errorMessage.includes("Access Denied: user is not the contract deployer!")
+    ) {
+      return "Access Denied: The address calling this function is not the deployer!";
+    } else if (
+      errorMessage.includes(
+        "Access Denied: counterPhase is not at Initialized!"
+      )
+    ) {
+      return "Access Denied: Counter has not been initialized!";
+    } else {
+      return "Unexpected Error!";
+    }
+  },
+
+  abi: [
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "int256",
+          "name": "n",
+          "type": "int256"
+        }
+      ],
+      "name": "decrement",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "get",
+      "outputs": [
+        {
+          "internalType": "int256",
+          "name": "",
+          "type": "int256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "int256",
+          "name": "n",
+          "type": "int256"
+        }
+      ],
+      "name": "increment",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "int256",
+          "name": "x",
+          "type": "int256"
+        }
+      ],
+      "name": "initialize",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ],
+};
+
+$(function () {
+  $(window).load(function () {
+    App.init();
+    toastr.options = {
+      closeButton: true,
+      debug: false,
+      newestOnTop: false,
+      progressBar: false,
+      positionClass: "toast-bottom-full-width",
+      preventDuplicates: false,
+      onclick: null,
+      showDuration: "300",
+      hideDuration: "1000",
+      timeOut: "5000",
+      extendedTimeOut: "1000",
+      showEasing: "swing",
+      hideEasing: "linear",
+      showMethod: "fadeIn",
+      hideMethod: "fadeOut",
+    };
+  });
+});
+
+/* Detect when the account on metamask is changed */
+window.ethereum.on("accountsChanged", () => {
+  App.populateAddress();
+});
+
+/* Detect when the network on metamask is changed */
+window.ethereum.on("chainChanged", () => {
+  App.populateAddress();
+});
