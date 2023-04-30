781cd3327249d79fae62bdadf6927bc9dd104ab7
vrashi <vrashrivastava@gmail.com>
2023-04-30 14:55:18 -0400

    corrected deployment issue

--- contracts/snap_sc.sol
+++ contracts/snap_sc.sol
@@ -161,7 +161,6 @@ contract SNAP is ERC721{
     {
         require(participants[applicant].provisionChangeRequested, "No provision change has been requested for this applicant");
         participants[applicant].provisionChangeRequested = false;
-        participants[applicant].openRequestMeal = '';
         emit ProvisionChangeDenied(applicant, participants[applicant].balance);
     }
     
